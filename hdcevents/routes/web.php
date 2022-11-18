fbae4ef45d9223425fa98a50cb7a85f39db42b00
helberte costa arruda <helberteads@gmail.com>
2022-11-17 20:54:05 -0400

    Instalação do jetstream

--- hdcevents/routes/web.php
+++ hdcevents/routes/web.php
@@ -73,3 +73,12 @@ Route::get('/produtos_param_query', function(){
 });
 
 */
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
